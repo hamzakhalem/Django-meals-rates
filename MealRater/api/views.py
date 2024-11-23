cea3b8a14f7433328a603486362315cdb034f854
hamzakhalem <hamzakhalem97@gmail.com>
2024-11-23 11:35:54 +0100

    feat: init commit

--- MealRater/api/views.py
+++ MealRater/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Meal, Rating
+from .serializers import MealSerializer, RatingSerializer
+
+
+class MealViewSet(viewsets.ModelViewSet):
+    queryset = Meal.objects.all()
+    serializer_class = MealSerializer
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+ 
