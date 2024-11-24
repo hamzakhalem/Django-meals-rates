8a04765e9feb6b06fe3363271e6038ba0256b0cd
hamzakhalem <hamzakhalem97@gmail.com>
2024-11-24 10:30:42 +0100

    feat: init commit

--- MealRater/api/views.py
+++ MealRater/api/views.py
@@ -1,12 +1,41 @@
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Meal, Rating
 from .serializers import MealSerializer, RatingSerializer
-
+from django.contrib.auth.models import User
 
 class MealViewSet(viewsets.ModelViewSet):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
+
+    @action(methods='POST', detail=True)
+    def rate_meal(self, request, pk=None):
+        if 'stars' in request.data:
+            meal = Meal.objects.get(id=pk)
+            stars = request.data['stars']
+            username = request.data['username']
+            user = User.objects.get(username=username)
+            try:
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                json = {
+                    'message': 'Meal Rate Updated',
+                    'result': serializer.data
+                }
+                return Response(json , status=status.HTTP_200_OK)
+
+            except:
+                # create if the rate not exist 
+                rating = Rating.objects.create(stars=stars, meal=meal, user=user)
+                serializer = RatingSerializer(rating, many=False)
+                json = {
+                    'message': 'Meal Rate Created',
+                    'result': serializer.data
+                }
+                return Response(json , status=status.HTTP_200_OK)
+        return Response({}, status= status.HTTP_400_BAD_REQUEST )
 
 
 class RatingViewSet(viewsets.ModelViewSet):
