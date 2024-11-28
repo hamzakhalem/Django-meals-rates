e837ab4e463b6a652b367740fcc5b7d734d24da7
hamzakhalem <hamzakhalem97@gmail.com>
2024-11-28 11:03:20 +0100

    fix: aruc bug for palns

--- MealRater/MealRater/settings.py
+++ MealRater/MealRater/settings.py
@@ -84,6 +84,12 @@ DATABASES = {
 }
 
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': {
+#         'rest_framework.permissions.IsAuhtenticated'
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
--- MealRater/api/views.py
+++ MealRater/api/views.py
@@ -5,8 +5,31 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
 
 from .models import Meal, Rating
-from .serializers import MealSerializer, RatingSerializer
+from .serializers import MealSerializer, RatingSerializer, UserSerializer
 from django.contrib.auth.models import User
+
+from rest_framework.authtoken.models import Token
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # #authentication_classes = (TokenAuthentication, )
+    permission_classes = (AllowAny,)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        token, created = Token.objects.get_or_create(user=serializer.instance)
+        return Response({
+                'token': token.key, 
+                }, 
+            status=status.HTTP_201_CREATED)
+    
+    def list(self, request, *args, **kwargs):
+        response = {'message': 'You cant create rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
 
 class MealViewSet(viewsets.ModelViewSet):
     queryset = Meal.objects.all()
